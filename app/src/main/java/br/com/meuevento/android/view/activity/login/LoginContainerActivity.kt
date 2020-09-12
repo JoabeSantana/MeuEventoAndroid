991fb45d61e4ca84bc56b0a754320949841ca7a8
Joabe Santana <joabesantana.c@gmail.com>
2020-09-12 16:19:15 -0300

    Criação dos fragmentos

---
+++ app/src/main/java/br/com/meuevento/android/view/activity/login/LoginContainerActivity.kt
@@ -0,0 +1,12 @@
+package br.com.meuevento.android.view.activity.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.meuevento.android.R
+
+class LoginContainerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_container)
+    }
+}
--- app/src/main/java/br/com/meuevento/android/viewmodel/LoginViewModel.kt
+++ app/src/main/java/br/com/meuevento/android/viewmodel/LoginViewModel.kt
@@ -1,14 +1,34 @@
 package br.com.meuevento.android.viewmodel
 
 import android.app.Application
-import android.content.Context
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
 import br.com.meuevento.android.interactor.LoginInteractor
-import kotlin.Exception
 
 class LoginViewModel(val app : Application) : AndroidViewModel(app) {
 
+    val LOGIN_SUCCESS = "LOGIN_SUCCESS"
+    val LOGIN_ERROR = "LOGIN_ERROR"
+
     private val loginInteractor = LoginInteractor()
+
+    val checkPassword = MutableLiveData<String>()
+    val password = MutableLiveData<String>()
+    val email = MutableLiveData<String>()
+
+    var result = MutableLiveData<String>()
+
+    fun login() {
+        loginInteractor.login(email.value.toString(), password.value.toString()) { error ->
+            result.value = if (error == null) LOGIN_SUCCESS else LOGIN_ERROR
+        }
+    }
+
+    fun signup() {
+        loginInteractor.signup(email.value.toString(), password.value.toString(), checkPassword.value.toString()) { error ->
+            result.value = if (error == null) LOGIN_SUCCESS else LOGIN_ERROR
+        }
+    }
 
     fun login(user: String, password: String, callback: (error: Exception?) -> Unit){
         //TODO: REGRAS DE APRESENTAÇÃO
